ab4384a70fc74ab4309c85ac5decacc1c7d6479e
Alexis Villegas Torres <alexisvt@gmail.com>
2019-05-08 23:17:48 -0600

    add redux and router packages to the app

---
+++ src/components/container/PetGrid.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+class PetGrid extends React {
+
+}
---
+++ src/store/actions/petActions.ts
@@ -0,0 +1,5 @@
+import { GET_ALL_PETS } from './actionTypes';
+
+export const getAllPets = () => {
+  return { type: GET_ALL_PETS };
+};
